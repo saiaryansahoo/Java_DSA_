4c110c5408ab6b90b809fb2bb39041a7c42bc55e
Sai Aryan Sahoo <jeetan.bdk@gmail.com>
2022-09-11 00:34:58 +0530

    IN

--- src/jeet/test.java
+++ src/jeet/test.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        System.out.print("Please enter your roll no:"+ "");
-        int rollno=input.nextInt();
-        System.out.println("Your Roll No. is" +" "+rollno);
+        System.out.print("Enter the First No:");
+        float no1=input.nextFloat();
+        System.out.print("Enter the Second No:");
+        float no2=input.nextFloat();
+        float sum=no1+no2;
+        System.out.println("The Sum is :" +" " + sum);
+
+
 
     }
 }
