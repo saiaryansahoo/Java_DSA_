e2f64a00bed96c4c1fed647c3ae5ed259f0854ff
Sai Aryan Sahoo <jeetan.bdk@gmail.com>
2022-09-11 20:20:22 +0530

    IN

--- src/jeet/firstproject.java
+++ src/jeet/firstproject.java
@@ -1,22 +1,38 @@
 package jeet;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class firstproject {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.print("Enter Your Salary:"+ " ");
-        int salary=input.nextInt();
+//        Scanner input=new Scanner(System.in);
+//        System.out.print("Enter Your Salary:"+ " ");
+//        int salary=input.nextInt();
+//
+//        if (salary>100000) {
+//            salary = salary + 50000;
+//            System.out.print("Net Salary is:" + " " + salary);
+//        }
+//
+//
+//        else if (salary<100000 && salary>50000) {
+//            salary=salary+30000;
+//            System.out.print("Salary is" + " "+ salary);
+//
+//        }
+//        else if (salary<50000 && salary>20000) {
+//            salary=salary+20000;
+//            System.out.print("Salary is" + " "+ salary);
+//
+//        }
+//        else{
+//            salary=salary+10000;
+//            System.out.println("Salary is" + " "+ salary);
+//            System.out.println("Work Hard");
+//        }
+        for (int num=1; num <=10; num+=1){
+            System.out.println(num);
 
-        if (salary>100000) {
-            salary=salary+50000;
-            System.out.print("Net Salary is:" + " " + salary);
         }
-        else if (salary<50000) {
-            salary=salary+15000;
-            System.out.print("Salary is" + " "+ salary);
-
-        }
-
     }
+
 }
