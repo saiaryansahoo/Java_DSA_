9502893451bc98ba4d2b3988cc0473daf51c244c
Sai Aryan Sahoo <jeetan.bdk@gmail.com>
2022-09-13 23:09:02 +0530

    IN

--- src/jeet/firstproject.java
+++ src/jeet/firstproject.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class firstproject {
     public static void main(String[] args) {
-//        Scanner input=new Scanner(System.in);
+       Scanner input=new Scanner(System.in);
 //        System.out.print("Enter Your Salary:"+ " ");
 //        int salary=input.nextInt();
 //
@@ -30,15 +30,46 @@ public class firstproject {
 //            System.out.println("Work Hard");
 //        }
 
-        for (int num=1; num <=10; num+=1) {
-            System.out.println("Hello World");
+//        for (int num=1; num <=10; num+=1) {
+//            System.out.println("Hello World");
+//
+//        }
 
-        }
+//        int num=1;
+//        while (num<=5){
+//            System.out.println(num);
+//            num+=1;
+//        }
 
+//        int n=1;
+//        do{
+//            System.out.print(n);
+//            n++;
+//        } while (n <=10);
+//        int a=input.nextInt();
+//        int b=input.nextInt();
+//        int c=input.nextInt();
 
+//        int max=a;
+//        if (b>max){
+//            max=b;
+//        }
+//        if(c>max){
+//            max=c;
+//        }
+//        int max = Math.max(a, b);
+//
+//        if (c>max){
+//          max=c;
+//        }
+//
+//
+//
+//
+//        System.out.println(max);
 
-
-
+        char ch=input.next().trim().charAt(0);
+        System.out.println(input.next().trim()  );
 
     }
 
