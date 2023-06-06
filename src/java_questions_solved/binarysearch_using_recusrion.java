b0c03eb0017053f33ccc08a282849dc06325b8b0
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-06 16:47:46 +0530

    IN

---
+++ src/java_questions_solved/binarysearch_using_recusrion.java
@@ -0,0 +1,22 @@
+package java_questions_solved;
+
+public class binarysearch_using_recusrion {
+    public static void main(String[] args) {
+        int[]arr={1,12,13,24,35,46};
+        int target=35;
+        System.out.println(find(arr,target, 0, arr.length-1));
+    }
+    static int find(int[] arr, int target,int s, int e){
+        if (s>e){
+            return -1;
+        }
+        int m=s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if (target<arr[m]){
+            return find(arr,target,s,m-1);
+        }
+        return find(arr, target,m+1,e);
+    }
+}
---
+++ src/java_questions_solved/ex.java
@@ -0,0 +1,18 @@
+package java_questions_solved;
+
+public class ex {
+    static int a;
+    static int b;
+    static void arya(){
+        a=12;
+        b=13;
+    }
+    public static void main(String[] args) {
+        arya();
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+
+}
