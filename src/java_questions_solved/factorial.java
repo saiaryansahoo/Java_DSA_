4423dd3cc8962a887afcf7bc156b26e5eec7e188
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-11 21:25:42 +0530

    IN

---
+++ src/java_questions_solved/factorial.java
@@ -0,0 +1,14 @@
+package java_questions_solved;
+
+public class factorial {
+    public static void main(String[] args) {
+        int ans=factorial(5);
+        System.out.println(ans);
+    }
+    static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+}
