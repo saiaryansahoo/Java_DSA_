ffc37588041d7e383aeb3595a01b4b8489f4d224
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-12 00:02:40 +0530

    IN

--- src/java_questions_solved/pallindrome.java
+++ src/java_questions_solved/pallindrome.java
@@ -1,4 +1,22 @@
 package java_questions_solved;
 
 public class pallindrome {
+    public static void main(String[] args) {
+        System.out.println(pallin(12345321));
+    }
+    static int rev(int n){
+        int digits=(int)(Math.log10(n)+1);
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem=n%10;
+        return rem*(int)(Math.pow(10, digits-1))+helper(n/10, digits-1);
+    }
+    static boolean pallin(int n){
+        return n==rev(n);
+    }
 }
