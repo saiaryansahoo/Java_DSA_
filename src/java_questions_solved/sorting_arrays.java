0e1f03a2590e5439b9f85b315109c5947fc14797
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-12 22:59:23 +0530

    IN

--- src/java_questions_solved/sorting_arrays.java
+++ src/java_questions_solved/sorting_arrays.java
@@ -2,9 +2,13 @@ package java_questions_solved;
 
 public class sorting_arrays {
     public static void main(String[] args) {
-
+        int[] arr={1,3,5,8};
+        System.out.println(sort(arr,0));
     }
-    static void sort(int n){
-
+    static boolean sort(int[] arr, int index){
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1]&& sort(arr,index+1);
     }
 }
