6bef1d0db0f514c38c1a0d0af5c77cfc88075ac6
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-13 16:56:11 +0530

    IN

--- src/java_questions_solved/sorting_arrays.java
+++ src/java_questions_solved/sorting_arrays.java
@@ -2,7 +2,7 @@ package java_questions_solved;
 
 public class sorting_arrays {
     public static void main(String[] args) {
-        int[] arr={1,3,5,8};
+        int[] arr={1,3,5,8,4};
         System.out.println(sort(arr,0));
     }
     static boolean sort(int[] arr, int index){
