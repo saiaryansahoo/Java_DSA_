bd7c09e13a4e358f7125875ae6d7849ebf50e03b
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-15 22:47:57 +0530

    IN

--- src/java_questions_solved/mergesort_recusion.java
+++ src/java_questions_solved/mergesort_recusion.java
@@ -1,7 +1,52 @@
 package java_questions_solved;
 
+import java.util.Arrays;
+
 public class mergesort_recusion {
     public static void main(String[] args) {
-
+        int[]arr={54,21,13,1,4};
+        int ans=merge(arr);
+        System.out.println(Arrays.toString(arr));
     }
+    static int[] merge(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid=arr.length/2;
+        int[] left=merge((Arrays.copyOfRange(arr,0, mid)));
+        int[] right=merge((Arrays.copyOfRange(arr,mid, arr.length)));
+
+        return merge(left,right);
+    }
+
+    private static int merge(int first, int second) {
+        int mix=new int[first.length+second.length];
+
+        int i=0;
+        int j=0;
+
+        while ((i<first.length && j< second.length)){
+            if(first[i]<second[j]){
+                mix[k]=first[i];
+                i++;
+            }
+            else {
+                mix[k]=second[j];
+                j++;
+            }
+            k++;
+        }
+        while ((i<first.length)){
+            mix[k]=first[i];
+            i++;
+            k++;
+        }
+        while (j<second.length){
+            mix[k]=second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+
 }
