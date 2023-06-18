31dd105570d7e712e4bff0b121c1420476c0570d
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-18 19:42:10 +0530

    IN

--- src/java_questions_solved/lengthof_lastword.java
+++ src/java_questions_solved/lengthof_lastword.java
@@ -2,6 +2,20 @@ package java_questions_solved;
 
 public class lengthof_lastword {
     public static void main(String[] args) {
-
+        String str=" fly to the moon ";
+        System.out.println(lengthOfLastWord(str));
+    }
+    static int lengthOfLastWord(String s){
+        String str=s.trim();
+        int count=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;
     }
 }
