0bacc2e4c0cae7e8de1a23f07bbc6d677dba8192
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-19 21:45:13 +0530

    IN

---
+++ src/java_questions_solved/largestAltitude.java
@@ -0,0 +1,17 @@
+package java_questions_solved;
+
+public class largestAltitude {
+    public static void main(String[] args) {
+        int[] gain={-5,1,5,0,-7};
+        System.out.println(largestAltitude(gain));
+    }
+    static int largestAltitude(int[] gain){
+        int max=0;
+        int current=0;
+        for (int i=0; i<gain.length;i++){
+            current +=gain[i];
+            max=Math.max(current,max);
+        }
+        return max;
+    }
+}
