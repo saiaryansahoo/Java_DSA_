32aee68a9074876dee09aa074079f0de0d68e0cb
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-20 16:42:49 +0530

    IN

--- src/java_questions_solved/prime_number.java
+++ src/java_questions_solved/prime_number.java
@@ -1,7 +1,6 @@
 package java_questions_solved;
 
 import java.util.Scanner;
-
 public class prime_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
