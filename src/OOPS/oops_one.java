c8c42f66f51d01719277f54adb0ae873963707eb
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-21 19:55:44 +0530

    IN

--- src/OOPS/oops_one.java
+++ src/OOPS/oops_one.java
@@ -4,12 +4,20 @@ import java.util.Arrays;
 
 public class oops_one {
     public static void main(String[] args) {
-        Student[] students=new Student[5];
+//        Student[] students=new Student[5];//new keyword is used for dynamic memory allocation
 
-        Student Aryan;
-        System.out.println(Arrays.toString(students));
+//        Student Aryan;
+//        System.out.println(Student(12,"Aryan",12.4));
+
+        Student Aryan=new Student();
+        Aryan.rno=10975;
+        Aryan.name="Aryan";
+        Aryan.marks=8.14f;
+        System.out.println(Aryan.rno);
+        System.out.println(Aryan.name);
+        System.out.println(Aryan.marks);
     }
-    class Student{
+    static class Student{
         int rno;
         String name;
         float marks;
