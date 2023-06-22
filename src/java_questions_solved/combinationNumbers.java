bc8efdc72843c4dcd643b72d8f974a6fee047c09
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-06-22 17:32:49 +0530

    IN

--- src/java_questions_solved/combinationNumbers.java
+++ src/java_questions_solved/combinationNumbers.java
@@ -1,51 +1,31 @@
-package java_questions_solved;
-
 import java.util.Scanner;
 
 public class combinationNumbers {
-    static int phi(int n)
-    {
-        // Initialize result as n
+    static int phi(int n) {
         int result = n;
 
-        // Consider all prime factors
-        // of n and subtract their
-        // multiples from result
-        //for (int i = 0; i < array.length; i++) {
-
-
-        for (int p = 2; p * p <= n; p++)
-        {
-
-            // Check if p is
-            // a prime factor.
-            while (n % p ==0)
-            {
-
-                // If yes, then update
-                // n and result
-                if (n % p == 0)
-                    n =n/p;
-                result = result-result / p;
+        for (int p = 2; p * p <= n; p++) {
+            if (n % p == 0) {
+                while (n % p == 0) {
+                    n = n / p;
+                }
+                result = result - result / p;
             }
         }
 
-        // If n has a prime factor
-        // greater than sqrt(n)
-        // (There can be at-most
-        // one such prime factor)
         if (n > 1)
-            result = result- result  /n;
+            result = result - result / n;
+
         return result;
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        scanner.close();
 
-    public static void main (String[] args)
-    {
-        //	int n=1000;
-        Scanner n1 = new Scanner(System.in);
-        System.out.print("Combination Number's Upto: ");
-        int n = n1.nextInt();
-        System.out.println("Total No's of Combination Number's upto: "+ phi(n));
+        int eulerTotient = phi(n);
+        System.out.println("Euler's Totient Function for " + n + ": " + eulerTotient);
     }
 }
