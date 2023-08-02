5dc2d929e5a207fa2c31e3506ad81b6870a60048
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-08-02 23:35:30 +0530

    IN

--- src/java_questions_solved/Bulls_And_Cows.java
+++ src/java_questions_solved/Bulls_And_Cows.java
@@ -1,4 +1,10 @@
 package java_questions_solved;
 
 public class Bulls_And_Cows {
+    public static void main(String[] args) {
+
+    }
+    static String getHint(String secret, String guess){
+        
+    }
 }
