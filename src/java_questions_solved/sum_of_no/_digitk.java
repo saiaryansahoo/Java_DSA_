7c16aad021e01acce38581e8a73750b51fc9b12d
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-11-20 20:39:07 +0530

    IN

---
+++ src/java_questions_solved/sum_of_no/_digitk.java
@@ -0,0 +1,30 @@
+package java_questions_solved.sum_of_no;
+
+public class _digitk {
+    public static void main(String[] args) {
+        class Solution {
+            // TC : O(1)
+            // SC : O(1)
+            public int minimumNumbers(int num, int k) {
+                if(num==0) {
+                    return 0;
+                }
+
+                if(k==0)  {
+                    return num%10==0 ? 1 : -1;
+                }
+
+
+                for(int i=1; i*k<=num && i<=10; i++)
+                {
+                    if((num - i*k)%10==0)  {
+
+                        return i;
+                    }
+                }
+                return -1;
+
+            }
+        }
+    }
+}
