1c78d90108a3abe0361a4ffc358414d3a251ce2e
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-01 00:17:49 +0530

    IN

---
+++ src/OOPS/packages_with_importusage/a/Hello.java
@@ -0,0 +1,10 @@
+package OOPS.packages_with_importusage.a;
+
+import static OOPS.packages_with_importusage.b.Greetings.greet;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hola! Aryan this side :)");
+        greet();
+    }
+}
---
+++ src/OOPS/packages_with_importusage/b/Greetings.java
@@ -0,0 +1,11 @@
+package OOPS.packages_with_importusage.b;
+
+public class Greetings {
+    public static void main(String[] args) {
+
+    }
+
+    public static void greet() {
+        System.out.println("How are you doing?");
+    }
+}
