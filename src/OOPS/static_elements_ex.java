8a84cd2556d4b647ace4bbd27b8aef61e403349e
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-01 00:18:14 +0530

    IN

---
+++ src/OOPS/static_elements_ex.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class static_elements_ex {
+    int age;
+    String name;
+    boolean Married;
+    static long population;
+
+    public static_elements_ex(int age, String name, boolean married) {
+        this.age = age;
+        this.name = name;
+        this.Married = married;
+        static_elements_ex.population +=1;
+    }
+}
