e5386d38c7f0a1bdc46ba4634b227eafb3df7f86
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-05 15:11:33 +0530

    IN

---
+++ src/java_questions_solved/groupAnagrams.java
@@ -0,0 +1,24 @@
+package java_questions_solved;
+
+import java.util.*;
+
+public class groupAnagrams {
+    public static void main(String[] args) {
+        String[] anagrams = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = solution(anagrams);
+
+        System.out.println(result);
+    }
+    public static List<List<String>> solution(String[] strs){
+        Map<String,List<String>> groups = new HashMap();
+
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            groups.computeIfAbsent(key, value -> new ArrayList<>()).add(word);
+        }
+        return new ArrayList<>(groups.values());
+    }
+}
