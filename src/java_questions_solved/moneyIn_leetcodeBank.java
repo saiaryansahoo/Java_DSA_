43dd892426285b45250593d21e2f1ded09903b62
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-06 12:56:17 +0530

    IN

---
+++ src/java_questions_solved/moneyIn_leetcodeBank.java
@@ -0,0 +1,20 @@
+package java_questions_solved;
+
+public class moneyIn_leetcodeBank {
+    public static void main(String[] args) {
+        int money=4;
+        System.out.println(totalMoney(money));
+    }
+    public static int totalMoney(int n) {
+        int m=n/7;
+        int res=0;
+
+        for(int i=1;i<=m;i++){
+            res+=7*(i+3);
+        }
+        for(int i=7*m;i<n;i++){
+            res+=++m;
+        }
+        return res;
+    }
+}
