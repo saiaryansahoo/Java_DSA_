03beeeb80a8bf54de4df115a1d5baa057bb4d2c2
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-08 00:22:17 +0530

    IN

--- src/java_questions_solved/largestODD_NoInString.java
+++ src/java_questions_solved/largestODD_NoInString.java
@@ -3,7 +3,6 @@ package java_questions_solved;
 public class largestODD_NoInString {
     public static void main(String[] args) {
         System.out.println(largestOddNumber("35427"));
-
     }
     public static String largestOddNumber(String num) {
         for (int i = num.length() - 1; i >= 0; i--) {
