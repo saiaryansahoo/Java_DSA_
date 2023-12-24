be7ad4d4317bf2ff498b334bb54c8bf39d416a79
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-24 21:25:06 +0530

    Update

--- src/DSA/Recursion/Q9.java
+++ src/DSA/Recursion/Q9.java
@@ -2,6 +2,14 @@ package DSA.Recursion;
 
 public class Q9 {
     public static void main(String[] args) {
+        //array sorted or not
+        int[] arr={1,3,2,5,4,6};
+        System.out.println(sorted(arr,0));
     }
-
+     static boolean sorted(int[] arr, int index ){
+        if(index== arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1] && sorted(arr,index+1);
+     }
 }
