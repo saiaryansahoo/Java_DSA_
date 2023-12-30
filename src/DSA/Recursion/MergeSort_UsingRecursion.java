d6b75fa57b90b9832b24150e43206b145b4889d6
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-30 23:38:59 +0530

    Update

---
+++ src/DSA/Recursion/MergeSort_UsingRecursion.java
@@ -0,0 +1,15 @@
+package DSA.Recursion;
+
+import java.util.Arrays;
+
+public class MergeSort_UsingRecursion {
+    public static void main(String[] args) {
+        int[] arr={8,3,4,12,5,6};
+    }
+    static int[] sort(int[] arr){
+        if(arr.length ==1){
+            return arr;
+        }
+        int[] left=sort(Arrays.copyOfRange())
+    }
+}
