96a59168636476032340545bbfe0e5a8b65a3277
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-01-15 19:31:59 +0530

    Update

--- src/java_questions_solved/DetermineTwo_StringsAreClose.java
+++ src/java_questions_solved/DetermineTwo_StringsAreClose.java
@@ -6,11 +6,11 @@ public class DetermineTwo_StringsAreClose {
     public static void main(String[] args) {
        String word1="abc";
        String word2="bca";
-       Solution19 sol=new Solution19();
+       Strings sol=new Strings();
         System.out.println(sol.closeStrings(word1, word2));
     }
 }
-class Solution19 {
+class Strings {
     public boolean closeStrings(String word1, String word2) {
         if (word1.length() != word2.length())
             return false;
