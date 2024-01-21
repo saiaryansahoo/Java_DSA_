0f1816ae9700b1c29798cb6f40d807b01c72ee38
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-01-21 23:52:32 +0530

    Update

--- src/java_questions_solved/MazeProblem_inAllDirections.java
+++ src/java_questions_solved/MazeProblem_inAllDirections.java
@@ -1,13 +1,16 @@
 package java_questions_solved;
 
+import java.util.Arrays;
+
 public class MazeProblem_inAllDirections {
     public static void main(String[] args) {
         boolean[][] board={
                 {true,true,true},
-                {false,false,true},
-                {true,true,false},
+                {true,true,true},
+                {true,true,true},
         };
-        allPath("",board,0,0);
+        int[][] paths=new int[board.length][board[0].length];
+        printAllPath("",board,0,0,paths,1);
     }
     static void allPath(String path,boolean[][] maze, int row, int column){
         if(row== maze.length-1 && column==maze[0].length-1){
@@ -17,6 +20,8 @@ public class MazeProblem_inAllDirections {
         if(!maze[row][column]){
             return;
         }
+
+        maze[row][column] = false;
 
         if(row<maze.length-1){
             allPath(path+'D',maze,row+1,column);
@@ -28,7 +33,43 @@ public class MazeProblem_inAllDirections {
             allPath(path+'U',maze,row-1,column);
         }
         if(column>0){
-            allPath(path+'D',maze,row,column-1);
+            allPath(path+'L',maze,row,column-1);
         }
+
+        maze[row][column] = true;
+    }
+
+    //printing all the paths with the matrix
+    static void printAllPath(String path,boolean[][] maze, int row, int column, int[][] paths, int step){
+        if(row== maze.length-1 && column==maze[0].length-1){
+            paths[row][column] = step;
+            for(int[]arr:paths){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(path);
+            System.out.println();
+            return;
+        }
+        if(!maze[row][column]){
+            return;
+        }
+
+        maze[row][column] = false;
+        paths[row][column] = step;
+        if(row<maze.length-1){
+            printAllPath(path+'D',maze,row+1,column, paths, step+1);
+        }
+        if(column< maze[0].length-1){
+            printAllPath(path+'R',maze, row,column+1, paths, step+1);
+        }
+        if(row>0){
+            printAllPath(path+'U',maze,row-1,column, paths, step+1);
+        }
+        if(column>0){
+            printAllPath(path+'L',maze,row,column-1, paths, step+1);
+        }
+
+        maze[row][column] = true;
+        paths[row][column] = 0;
     }
 }
