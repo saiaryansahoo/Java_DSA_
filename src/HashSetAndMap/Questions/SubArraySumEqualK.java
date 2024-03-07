5a9f0be47b447bd7284eb65faed7f5972591df07
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-03-07 12:46:39 +0530

    Update

---
+++ src/HashSetAndMap/Questions/SubArraySumEqualK.java
@@ -0,0 +1,29 @@
+package HashSetAndMap.Questions;
+
+import java.util.HashMap;
+
+public class SubArraySumEqualK {
+    public static void main(String[] args) {
+        int[] arr={10,2,-2,-20,10};
+        int k=-10;
+
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        int length= arr.length;
+        int ans=0;
+        int sum=0;
+        for (int j = 0; j < length; j++) {
+            sum += arr[j];
+
+            if(map.containsKey(sum-k)){
+                ans +=map.get(sum-k);
+            }
+            if(map.containsKey(sum)){
+                map.put(sum,map.get(sum)+1);
+            }else{
+                map.put(sum,1);
+            }
+        }
+        System.out.print(ans);
+    }
+}
--- src/HashSetAndMap/Questions/TicketItinerary.java
+++ src/HashSetAndMap/Questions/TicketItinerary.java
@@ -1,7 +1,36 @@
 package HashSetAndMap.Questions;
 
+import java.util.HashMap;
+
 public class TicketItinerary {
     public static void main(String[] args) {
+        HashMap<String,String> originalMap=new HashMap<String,String>();
+        originalMap.put("Chennai","Bengaluru");
+        originalMap.put("Mumbai","Delhi");
+        originalMap.put("Goa","Chennai");
+        originalMap.put("Delhi","Goa");
 
+        String start=getStart(originalMap);
+
+        while (originalMap.containsKey(start)){
+            System.out.print(start+ "->");
+            start=originalMap.get(start);
+        }
+        System.out.print(start);
+    }
+
+    public static String getStart(HashMap<String,String> map1){
+        HashMap<String,String> reverseMap=new HashMap<String,String>();
+        for(String key: map1.keySet()){
+            reverseMap.put(map1.get(key),key);
+        }
+
+
+        for(String key:map1.keySet()){
+            if(!reverseMap.containsKey(key)){
+                return key;
+            }
+        }
+        return null;
     }
 }
