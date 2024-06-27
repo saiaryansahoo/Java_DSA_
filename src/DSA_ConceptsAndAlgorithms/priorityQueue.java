00958efe31ef514ac2d63c0ed940e6a705341a01
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-06-27 23:38:30 +0530

    Update

---
+++ src/DSA_ConceptsAndAlgorithms/priorityQueue.java
@@ -0,0 +1,21 @@
+package DSA_ConceptsAndAlgorithms;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class priorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(1);
+        pq.add(2);
+        pq.add(4);
+        pq.add(0);
+        pq.add(11);
+
+        while(!pq.isEmpty()){
+            System.out.print(pq.peek()+"->");
+            pq.remove();
+        }
+    }
+}
