6c97def2c34d8a56ea90333e5224cfc784d18d67
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-08-04 23:48:58 +0530

    Update

---
+++ src/Solved_Questions/ThreeSum.java
@@ -0,0 +1,56 @@
+package Solved_Questions;
+
+import java.util.*;
+
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicate elements
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        List<List<Integer>> result = ThreeSum.threeSum(nums);
+
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
