eadc043c1b76ec3dfbbb87890b7f4cfd4190e6b8
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-08-27 23:55:47 +0530

    Update

--- src/Solved_Questions/validPasswordChar.java
+++ src/Solved_Questions/validPasswordChar.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class validPasswordChar {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
