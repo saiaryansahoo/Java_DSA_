80db5d5ade93e9ef2601efdf9c8b251edd667032
aryansahoo0912 <jeetan.bdk@gmail.com>
2024-09-03 15:58:07 +0530

    Update

---
+++ src/Solved_Questions/bobHalloween.java
@@ -0,0 +1,36 @@
+package Solved_Questions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bobHalloween {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] A=new int[n];
+
+        for (int i = 0; i < n; i++) {
+            A[i]=sc.nextInt();
+        }
+
+        int M=sc.nextInt();
+
+        int count=0;
+
+        Arrays.sort(A);
+
+        for (int i = 0; i < n; i++) {
+            if(A[i]%5==0){
+                count++;
+            }else if(M>=A[i]){
+                count++;
+                M=M-A[i];
+            }else{
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
